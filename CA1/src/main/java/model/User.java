08e606d1a84e5a0a3b807cdc29c193d65a33ad92
M.R. Bakhshayesh <baxayesh@gmail.com>
2024-02-26 17:19:56 +0330

    fix: remove username normalization from user class

--- CA1/src/main/java/model/User.java
+++ CA1/src/main/java/model/User.java
@@ -12,12 +12,8 @@ public class User extends EntityModel<String> {
     //fields: username password, email, address, role
     private UserRole Role;
 
-    static String NormalizeUsername(String username){
-        return  username.strip().toUpperCase();
-    }
-
     public User(String username, UserRole role) {
-        super(NormalizeUsername(username));
+        super(username);
         Role = role;
     }
 
@@ -27,11 +23,6 @@ public class User extends EntityModel<String> {
 
     public boolean RoleIs(UserRole desiredRole) {
         return Role == desiredRole;
-    }
-
-    @Override
-    public boolean Is(String otherKey) {
-        return super.Is(NormalizeUsername(otherKey));
     }
 
 }
