04d41012abafb1c0a13234ad5c897d77d2aa083a
M.R. Bakhshayesh <baxayesh@gmail.com>
2024-06-19 12:57:18 +0330

    Fix: revert accidentally made merge

--- CA9/apply-changes.bat
+++ CA9/apply-changes.bat
@@ -1,7 +1,6 @@
 kubectl apply -f mysql-pvc.yaml -n mishap
 kubectl apply -f mysql-deployment.yaml -n mishap
 kubectl apply -f mysql-service.yaml -n mishap
-timeout /t 180 /nobreak >nul
 kubectl apply -f backend-deployment.yaml -n mishap
 kubectl apply -f backend-service.yaml -n mishap
 kubectl apply -f frontend-deployment.yaml -n mishap
