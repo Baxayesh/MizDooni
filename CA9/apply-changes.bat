ea29daf027475859eb4a47f819df959ed5fc05f2
M.R. Bakhshayesh <baxayesh@gmail.com>
2024-06-19 11:07:07 +0330

    fix: fix bug with pvc

--- CA9/apply-changes.bat
+++ CA9/apply-changes.bat
@@ -1,15 +1,18 @@
-kubectl delete -f mysql-pvc.yaml -n mishap
-kubectl delete -f mysql-deployment.yaml -n mishap
-kubectl delete -f mysql-service.yaml -n mishap
-kubectl delete -f backend-deployment.yaml -n mishap
-kubectl delete -f backend-service.yaml -n mishap
-kubectl delete -f frontend-deployment.yaml -n mishap
 kubectl delete -f frontend-service.yaml -n mishap
+kubectl delete -f frontend-deployment.yaml -n mishap
+kubectl delete -f backend-service.yaml -n mishap
+kubectl delete -f backend-deployment.yaml -n mishap
+kubectl delete -f mysql-service.yaml -n mishap
+kubectl delete -f mysql-deployment.yaml -n mishap
+kubectl delete -f mysql-pvc.yaml -n mishap
 
 kubectl apply -f mysql-pvc.yaml -n mishap
 kubectl apply -f mysql-deployment.yaml -n mishap
 kubectl apply -f mysql-service.yaml -n mishap
+timeout /t 180 /nobreak >nul
 kubectl apply -f backend-deployment.yaml -n mishap
 kubectl apply -f backend-service.yaml -n mishap
 kubectl apply -f frontend-deployment.yaml -n mishap
 kubectl apply -f frontend-service.yaml -n mishap
+kubectl -n mishap port-forward svc/frontend 80:80
+kubectl -n mishap port-forward svc/backend
