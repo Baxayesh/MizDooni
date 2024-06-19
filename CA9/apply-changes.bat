d315be1881149ed9413abaeb17c3acefb84679c9
M.R. Bakhshayesh <baxayesh@gmail.com>
2024-06-19 11:45:53 +0330

    fix: remove delete commands from appy file

--- CA9/apply-changes.bat
+++ CA9/apply-changes.bat
@@ -1,11 +1,3 @@
-kubectl delete -f frontend-service.yaml -n mishap
-kubectl delete -f frontend-deployment.yaml -n mishap
-kubectl delete -f backend-service.yaml -n mishap
-kubectl delete -f backend-deployment.yaml -n mishap
-kubectl delete -f mysql-service.yaml -n mishap
-kubectl delete -f mysql-deployment.yaml -n mishap
-kubectl delete -f mysql-pvc.yaml -n mishap
-
 kubectl apply -f mysql-pvc.yaml -n mishap
 kubectl apply -f mysql-deployment.yaml -n mishap
 kubectl apply -f mysql-service.yaml -n mishap
